82220b7f7572ea69eff6b41f79a839b669a5b37d
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 05:11:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def player_numbers(team_name)
    
    game_hash.each do |teams, data|
     if data[:team_name] == team_name
-        array.push(data[:team_info])
+        array.push(data)
         binding.pry
       end
   end
