593b61f546b5623fd97e9d8c8a7a7e0cc2afa2ff
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 03:52:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,6 +17,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |team, data|
     data.each do |team_info, details|
+      if de
       details.each do |player, player_details|
         if player_name = player
           game_hash[team][data][team_info][details][points]
