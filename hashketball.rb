20dd1bb6871149f9b906155faec402a322354d03
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 04:30:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,6 +45,7 @@ def team_colors(team)
   game_hash.each do |teams, data|
     team_array << data[:team_name]
     end
+    binding.pry
     team_array.each do |index, teams|
       if teams[:team_name] == teams
         return teams[:colors]
